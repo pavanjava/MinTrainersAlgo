8b98d05a1a2811016bd9f708b6acec5d78f94762
H156759 <pavan.mantha@honeywell.com>
2021-06-13 08:36:30 +0530

    added function to create adjacency matrix

--- src/MinTainers.py
+++ src/MinTainers.py
@@ -6,6 +6,7 @@ class MinTrainers:
 
         self.total_uniq_subjects = []
         self.total_uniq_trainers = []
+        self.edges = []
 
     def read_input(self, input_file):
         try:
@@ -16,8 +17,11 @@ class MinTrainers:
                 line_data = line.split('/')
                 self.trainerSubjects.append((line_data[0], line_data[1:]))
 
+            self.trainerSubjects = sorted(self.trainerSubjects)
+
             self.create_uniq_list_of_subjects()
             self.create_uniq_list_of_trainers()
+            self.create_graph()
 
         except Exception as e:
             print(e)
@@ -28,24 +32,38 @@ class MinTrainers:
         for t in self.trainerSubjects:
             self.total_uniq_subjects.extend(x for x in t[1] if x not in self.total_uniq_subjects)
 
+        self.total_uniq_subjects = sorted(self.total_uniq_subjects)
+
     def create_uniq_list_of_trainers(self):
         for t in self.trainerSubjects:
             self.total_uniq_trainers.append(t[0])
-        self.total_uniq_trainers = list(set(self.total_uniq_trainers))
+        self.total_uniq_trainers = sorted(list(set(self.total_uniq_trainers)))
+
+    def create_graph(self):
+        self.edges = [[0 for i in range(len(self.total_uniq_trainers))] for j in range(len(self.total_uniq_subjects))]
+
+        for i in range(len(self.total_uniq_trainers)):
+            for j in range(len(self.total_uniq_subjects)):
+                if self.trainerSubjects[i][0] == self.total_uniq_trainers[i] and self.total_uniq_subjects[j] in \
+                        self.trainerSubjects[i][1]:
+                    self.edges[i][j] = 1
 
     def show_all(self):
         with open('outputPS13.txt', 'a') as f:
             f.write('\n----------Function showAll--------------\n')
-            f.write('Total no. of trainers: '+str(len(self.total_uniq_trainers))+'\n')
-            f.write('Total no. of subjects: '+str(len(self.total_uniq_subjects))+'\n')
+            f.write('Total no. of trainers: ' + str(len(self.total_uniq_trainers)) + '\n')
+            f.write('Total no. of subjects: ' + str(len(self.total_uniq_subjects)) + '\n')
 
             f.write('\nList of trainers:\n')
             for trainer in self.total_uniq_trainers:
-                f.write(trainer+'\n')
+                f.write(trainer + '\n')
 
             f.write('\nList of subjects:\n')
             for subject in self.total_uniq_subjects:
-                f.write(subject+'\n')
+                f.write(subject + '\n')
+
+            for i in self.edges:
+                print(i)
 
 
 def main():
